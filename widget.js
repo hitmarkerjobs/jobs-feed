1c40caf51c7d8b1a9b13b69ecf78794f844c2933
Craig Horsborough <craighorsborough@gmail.com>
2018-03-27 15:28:27 +0100

    adding a little better styles

--- widget.js
+++ widget.js
@@ -91,6 +91,7 @@
           logoImg.src = jobs[i].imageUrl;
           logoImg.width = '32';
           logoImg.height = '32';
+          logoImg.class = 'job-logo';
 
           logoWrapper.appendChild(logoImg);
           jobWrapper.appendChild(logoWrapper);
